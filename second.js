5abf0b74166f00e2854bf81e76f2d42f7a9b4837
Shraboni Devi <sraboni0723@gmail.com>
2025-03-06 14:06:12 +0600

    Create second.js
    js operators

---
+++ second.js
@@ -0,0 +1,19 @@
+//Arithmatc Operators
+let a=50;
+let b=56;
+console.log("a+b=",a+b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+//Unary Operator
+let p=20;
+let q=10;
+console.log("a+1=",a+1);
+console.log("++a=",++a);
+console.log("a++=",a+);
+//Assignment operators
+p+=4
+console.log("p+=",p);
+//Conditional statement
+let cgpa=3.7;
+if(cgpa>=3.8){
+    console.log("can apply");}
