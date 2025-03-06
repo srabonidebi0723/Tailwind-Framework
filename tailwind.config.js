6032d179c35cb31ff56ead8317205bf07e9f75f2
Shraboni Devi <sraboni0723@gmail.com>
2025-03-06 12:33:12 +0600

    Create tailwind.config.js
    javascript variables

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+name="Brainstation23";
+console.log(name)
+address="plot-8,mohakhali,Dhaka-1202"
+console.log(address)
+vacancy=null;
+console.log(null)
+const PI=3.14
+console.log(PI)
+{
+    let a=5;
+    console.log(a);
+}
+{
+    let a=10;
+    console.log(a);
+}
+
+let x=BigInt("123");
+const Brainstation23={
+    purpose:"Attachment",
+    Day: "10",
+};
+console.log(Brainstation23["purpose"]);
